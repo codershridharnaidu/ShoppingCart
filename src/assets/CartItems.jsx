4389ab2acca5d7845427f32f7b5f5de8c020fbf6
shridhar2003 <shridharnaiduedu@gmail.com>
2026-02-07 15:06:38 +0530

    Updated CartItems, cartSlices, store

--- src/assets/CartItems.jsx
+++ src/assets/CartItems.jsx
@@ -1,5 +1,82 @@
-function Cartitems() {
-  return <h1>Cart Page</h1>;
+import React from "react";
+import { Container, Button } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { LiaRupeeSignSolid } from "react-icons/lia";
+import { useNavigate } from "react-router-dom";
+
+function Carsitems() {
+  const { items, totalPrice } = useSelector((state) => state.cart);
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <h3 className="text-center my-4">Cart Items</h3>
+
+      {items.length > 0 ? (
+        <table className="table m-4">
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th>Remove</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {items.map((p) => (
+              <tr key={p.id}>
+                <td>
+                  <img
+                    src={p.image}
+                    alt={p.title}
+                    height={80}
+                    width={80}
+                  />
+                </td>
+                <td>{p.title}</td>
+                <td>{p.quantity}</td>
+                <td>
+                  <LiaRupeeSignSolid />
+                  {p.price * p.quantity}
+                </td>
+                <td>
+                  <Button variant="danger" size="sm">
+                    Remove
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+
+          <tfoot>
+            <tr>
+              <td colSpan={3} className="text-end fw-bold">
+                Total Amount:
+              </td>
+              <td colSpan={2} className="fw-bold">
+                <LiaRupeeSignSolid />
+                {totalPrice}
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      ) : (
+        <>
+          <h2 className="text-center">Please Add Products in Cart</h2>
+          <Button
+            variant="primary"
+            className="mx-auto d-block"
+            onClick={() => navigate("/")}
+          >
+            Continue Shopping
+          </Button>
+        </>
+      )}
+    </Container>
+  );
 }
 
-export default Cartitems;
+export default Carsitems;
+  
--- src/cartSlice.jsx
+++ src/cartSlice.jsx
@@ -8,18 +8,33 @@ const cartSlice = createSlice({
   },
   reducers: {
     addToCart: (state, action) => {
-        let existingItem = state.items.find(s=>s.id==action.payload.id)
-        if(existingItem){
-            existingItem.quantity+=1;
-        }
-        else{
-            state.items.push({...action.payload,quantity:1})
-        }
-        state.totalPrice = state.items.reduce((total,item)=>total+item.price*item.quantity,0);
-        state.tempItems=[...state.items]
-    }
+      let existingItem = state.items.find(s => s.id == action.payload.id)
+      if (existingItem) {
+        existingItem.quantity += 1;
+      }
+      else {
+        state.items.push({ ...action.payload, quantity: 1 })
+      }
+      state.totalPrice = state.items.reduce((total, item) => total + item.price * item.quantity, 0);
+      state.tempItems = [...state.items]
+    },
+    updateQuantity: (state, action) => {
+      let tItem = state.tempItems.find(t => t.id === action.payload.id)
+      if (tItem) {
+        tItem.quantity = action.payload.quantity;
+      }
+    },
+    applyChanges: (state, action) => {
+      let tItem = state.tempItems.find(c => c.id === action.payload.id)
+      const cItem = state.items.find(c => c.id === action.payload.id)
+      if (tItem && cItem) {
+        cItem.quantity = tItem.quantity;
+      }
+      state.totalPrice = state.items.reduce((total, item) => total + item.price * item.quantity, 0);
+    },
   }
-})
+}
+)
 
 export const { addToCart } = cartSlice.actions
 export default cartSlice.reducer
